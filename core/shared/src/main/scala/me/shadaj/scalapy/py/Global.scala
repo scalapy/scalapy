bad2689280d717d3c6dac3f4d500b321bc1fc946
Bruno Fernandes <8037438+bfdes@users.noreply.github.com>
2019-10-25 19:05:37 +0100

    Support calling by positional and keyword arguments (#38)

--- core/shared/src/main/scala/me/shadaj/scalapy/py/Global.scala
+++ core/shared/src/main/scala/me/shadaj/scalapy/py/Global.scala
@@ -3,12 +3,16 @@ package me.shadaj.scalapy.py
 import scala.language.dynamics
 
 object global extends scala.Dynamic {
-  def applyDynamic(method: String)(params: Any*): Any = {
-    Any.populateWith(CPythonInterpreter.callGlobal(method, params.map(_.value))).as[Dynamic]
+  def applyDynamic(method: String)(params: Any*): Dynamic = {
+    // Trick to distinguish positional Python arguments in Scala follows
+    val tupled = params.map(value => ("", value))
+    applyDynamicNamed(method)(tupled: _*)
   }
 
   def applyDynamicNamed(method: String)(params: (String, Any)*): Dynamic = {
-    eval(s"$method(${params.map(t => s"${t._1} = ${t._2.expr.variable}").mkString(",")})")
+    eval(s"$method(${params.map{ case (name, value) =>
+      if(name.isEmpty) Arg(value) else Arg(name, value)  // Positional arguments have no name
+    }.mkString(",")})")
   }
 
   def selectDynamic(value: String): Any = {
