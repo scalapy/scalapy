32475675e2ab308824619638a893e9f021a3e2e0
Mark Melton <mark.melton@notlem.com>
2020-03-05 14:47:03 -0600

    Cleaned up test

--- core/jvm/src/test/scala/me/shadaj/scalapy/ReferenceCountStressTest.scala
+++ core/jvm/src/test/scala/me/shadaj/scalapy/ReferenceCountStressTest.scala
@@ -3,46 +3,25 @@ package me.shadaj.scalapy.py
 import org.scalatest.funsuite.AnyFunSuite
 
 class ReferenceCountStressTest extends AnyFunSuite {
-  // val gc = py.module("gc")
-  // val sys = py.module("sys")
-  // def referenceCount(obj: py.Dynamic): Int = sys.getrefcount(obj).as[Int]
+  val gc = module("gc")
+  val sys = module("sys")
+  def referenceCount(obj: Dynamic): Int = sys.getrefcount(obj).as[Int]
 
   test("Repeated global function calls maintain constant reference counts") {
-    println("ComputePython...")
+    local  {
+      val sliceBase = global.slice(0)
+      val baseCount = referenceCount(sliceBase)
 
-    val gc = module("gc")
-    val sys = module("sys")
-
-    def referenceCounts(obj: Dynamic): (Int, Int) =
-      (sys.getrefcount(obj).as[Int], global.len(gc.get_referrers(obj)).as[Int])
-
-    val s0 = global.slice(0)
-      (0 to 2).foreach { index =>
+      (0 to 10).foreach { _ =>
         System.gc()
         System.runFinalization()
         gc.collect()
-        println(s"$index : counts: ${referenceCounts(s0)}")
+
+        val slice = global.slice(0)
+        val count = referenceCount(slice)
+
+        assert(baseCount == count)
       }
-
-    System.gc()
-    System.runFinalization()
-    gc.collect()
-    println(s"counts: ${referenceCounts(s0)}")
-
-    // py.local  {
-    //   val sliceBase = py.global.slice(0)
-    //   val baseCount = referenceCount(sliceBase)
-
-    //   (0 to 4).foreach { _ =>
-    //     System.gc()
-    //     System.runFinalization()
-    //     gc.collect()
-
-    //     val slice = py.global.slice(0)
-    //     val count = referenceCount(slice)
-
-    //     assert(baseCount == count)
-    //   }
-    // }
+    }
   }
 }
