7e9c860624c5966608bd65bf48cebce102539620
Shadaj Laddad <shadaj@users.noreply.github.com>
2020-04-06 17:44:26 -0700

    Handle multithreaded access to the interpreter by locking the GIL (#64)
    * Ensure that variable references don't use the same ID even if threads are involved
    
    * Stabilize initial reference count before running assertions
    
    * Wrap all Python API calls inside GIL lock
    
    * Fix native linking crash
    
    * Add stress test for multithreaded access to Python objects
    
    * Reduce length of reference counting stress test
    
    * Release the GIL even if an exception occurs
    
    * Wait for reference counts to settle

---
+++ core/jvm/src/test/scala/me/shadaj/scalapy/py/ThreadStressTest.scala
@@ -0,0 +1,23 @@
+package me.shadaj.scalapy.py
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class ThreadStressTest extends AnyFunSuite {
+  test("Accessing the same list from many different threads works") {
+    val list = global.list()
+
+    val threads = (1 to 5000).map { i =>
+      val t = new Thread(() => {
+        list.append(i)
+      })
+
+      t.start()
+
+      t
+    }
+
+    threads.foreach(_.join())
+
+    assert(list.as[Seq[Int]].sorted == (1 to 5000).toSeq)
+  }
+}
