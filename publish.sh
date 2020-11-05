a64f8c54a0f4b6d607ffce54b1d06c83740f57a2
Shadaj Laddad <shadaj@users.noreply.github.com>
2020-11-05 13:14:21 -0800

    Pull all tags to fix dynamic versioning in Travis

--- publish.sh
+++ publish.sh
@@ -2,6 +2,8 @@
 
 set -e # exit with nonzero exit code if anything fails
 
+git fetch --unshallow
+
 openssl aes-256-cbc -K $encrypted_876a1fdcb9d7_key -iv $encrypted_876a1fdcb9d7_iv -in secrets.tar.enc -out secrets.tar -d
 
 tar xvf secrets.tar
