ccf5bd2e3208f295d2d554dee3386793ab5d09f8
Shadaj Laddad <shadaj@users.noreply.github.com>
2022-04-17 22:15:52 +0000

    Move Scala 3 annotation definitions to the core package
    Fixes #243

--- core/shared/src/main/scala-3/me/shadaj/scalapy/py/FacadesMacros.scala
+++ core/shared/src/main/scala-3/me/shadaj/scalapy/py/FacadesMacros.scala
@@ -1,10 +1,15 @@
 package me.shadaj.scalapy.py
 
+import scala.annotation.StaticAnnotation
+
 trait FacadesCreatorMacros {
   inline implicit def getCreator[F <: Any]: FacadeCreator[F] = ${FacadeImpl.creator[F]}
 }
 
 trait PyMacros {
+  class PyBracketAccess extends StaticAnnotation
+  class native extends StaticAnnotation
+
   inline def native[T]: T = ${FacadeImpl.native_impl[T]}
   inline def nativeNamed[T]: T = ${FacadeImpl.native_named_impl[T]}
 }
